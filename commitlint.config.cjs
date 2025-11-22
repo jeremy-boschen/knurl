4ecc5ff47e43c1bf039cbbb71e4b20ef36eb1178
Jeremy Boschen <jeremy@newty.coffee>
2025-11-22 11:02:07 -0500

    chore: increase commitlint body max line length to 120 characters

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'subject-case': [2, 'never', ['sentence-case']],
+    'body-max-line-length': [2, 'always', 120],
   },
 };
 
