7a46b01c9d36e3964f840a3d298e59b6ec8b71da
Jeremy Boschen <jeremy@newty.coffee>
2025-12-04 09:30:02 -0500

    WIP

--- src-ui/src/components/icons/restore-icon.tsx
+++ src-ui/src/components/icons/restore-icon.tsx
@@ -1,4 +1,4 @@
-export function RestoreIcon({ className }: { className?: string }) {
+export function RestoreIcon({className}: { className?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -6,13 +6,25 @@ export function RestoreIcon({ className }: { className?: string }) {
       fill="none"
       stroke="currentColor"
       strokeWidth="1.5"
+      strokeLinejoin="round"
+      shapeRendering="geometricPrecision"
       className={className}
     >
       <title>Restore</title>
-      {/* Top-right rectangle (smaller) */}
-      <rect x="9" y="3" width="12" height="8" rx="0.5" />
-      {/* Bottom-left rectangle (larger) */}
-      <rect x="3" y="9" width="12" height="12" rx="0.5" />
+      <defs>
+        <mask id="restore-cutout">
+          {/* White = visible, Black = hidden */}
+          <rect width="24" height="24" fill="white"/>
+          {/* Slightly larger than the bottom rect to avoid AA fringe */}
+          <rect x="2" y="8" width="14" height="14" fill="black"/>
+        </mask>
+      </defs>
+
+      {/* Top-right rectangle, masked so it doesn't overlap */}
+      <rect x="6" y="6" width="12" height="12" rx="0.5" mask="url(#restore-cutout)"/>
+
+      {/* Bottom-left rectangle */}
+      <rect x="3" y="9" width="12" height="12" rx="0.5"/>
     </svg>
   )
 }
